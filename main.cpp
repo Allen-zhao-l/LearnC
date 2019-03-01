83ca470eb3d65b015edd8457fbfef4f3b6cf30d0
tony <982358010@qq.com>
2019-03-01 11:41:46 +0800

    完善递归结构算法

--- main.cpp
+++ main.cpp
@@ -4,18 +4,6 @@
 #include <memory>
 int main()
 {
-    Node<int> *node=new Node<int>;
-    Node<int> *first=node;
-    for (int x=0;x<10;++x)
-    {   
-        node->data=x*x;
-        node->next=new Node<int>;
-        node=node->next;
-    }
-    positive(first);
-    delfirst(first,16);
-    positive(first);
-
     return 0;
 }
 
--- src/recursion.cpp
+++ src/recursion.cpp
@@ -10,6 +10,7 @@ double avg_recu(_T &&array,int size,int n)
     else
         return (array[n])/size +avg_recu(array,size,n-1);
 }
+
 template<class _T>
 struct Node{
     _T data;
@@ -21,6 +22,7 @@ struct Node{
         return os;
     }
 };
+
 template<class _T>
 int count_num(Node<_T> *list)
 {
@@ -29,15 +31,17 @@ int count_num(Node<_T> *list)
     else
         return 1;
 }
+
 template<class _T>
 void positive(Node<_T> *list)
 {
-    if (list->next!=nullptr)
+    if (list!=nullptr)
     {
         std::cout<<list->data<<std::endl;
         positive(list->next);
     };
 }
+
 template<class _T>
 void native(Node<_T> *list)
 {
@@ -47,17 +51,57 @@ void native(Node<_T> *list)
         std::cout<<list->data<<std::endl;
     }
 }
+
+
 template<class _T>
-void delfirst(Node<_T> *list,_T data)
+void delfirst(Node<_T> *&list,_T data)
 {
-    if (list!=nullptr and list->data==data)
+    if (list==nullptr)
+        return;
+    if (list->data==data){
+        Node<_T> * temp=list;
         list=list->next;
-    if (list->next)
-        return delfirst(list->next,data);
+        delete temp;
+        return;
+    }
+    delfirst(list->next,data);
 }
 template<class _T>
-void delall(Node<_T> *list,_T data)
+void delall(Node<_T> *&list,_T data)
 {
+    if (list==nullptr)
+        return;
+    if (list->data==data){
+        Node<_T> * temp=list;
+        list=list->next;
+        delete temp;
+        delall(list,data);
+    }else
+        delall(list->next,data);
+}
 
+template<typename  _T>
+_T max(Node<_T> *list)
+{
+    _T ma=0;
+    if (list==nullptr)
+        return 0;
+    else
+    {
+        ma=max(list->next);
+        return ma>list->data?ma:list->data;
+    }
+}
+template<typename  _T>
+_T min(Node<_T> *list)
+{
+    _T mi=0;
+    if (list==nullptr)
+        return 0;
+    else
+    {
+        mi=min(list->next);
+        return mi<list->data?mi:list->data;
+    }
 }
 #endif
